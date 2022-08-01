4cc9809c3929e6470432afe069133ec24bbba1af
Aleksey Kosin <akosin@udm.ru>
2022-08-01 16:07:04 +0400

    correction flake mistakes

--- brain_games/game_even_numbers.py
+++ brain_games/game_even_numbers.py
@@ -8,23 +8,19 @@ def game_even_logic():
     print('Hello, ' + user_name)
     game_iterations = 0
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
     while game_iterations < 3:
         question_number = random.randint(1, 100)
         print('Question: ' + str(question_number))
         user_answer = prompt.string('Your answer: ')
-
         if question_number % 2 == 0:
             correct_answer = 'yes'
         else:
             correct_answer = 'no'
-        
         if user_answer == correct_answer:
             game_iterations += 1
-            print('Correct!')       
+            print('Correct!')
         else:
-            print(user_answer + ' is wrong answer ;(. Correct answer was ' + correct_answer+'.')
+            print(user_answer + ' is wrong answer ;(. Correct answer was ' + correct_answer + '.')
             print('Let\'s try again, ' + user_name + '!')
-            
             return
     print('Congratulations, ' + user_name + '!')
